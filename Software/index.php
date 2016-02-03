90a888ff72c05edafeac02e336dd2934249e7c3a
Zhen Wei <zwei@ivani.com>
2016-02-03 01:24:42 -0600

    start working on the Software

---
+++ Software/index.php
@@ -0,0 +1,9 @@
+<?php
+	include("menu.php");
+?>
+<html>
+	<head>
+		<title>Robot Web Control GUI</title>
+	</head>
+	<body></body>
+</html>
---
+++ Software/menu.php
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<a href="index.php">Home</a>
+		<a href="remoteController.php">Remote Controller</a>
+		<a href=""></a><br/>
+	</body>
+</html>
