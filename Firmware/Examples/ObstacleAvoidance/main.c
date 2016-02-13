01965e2ce2c98e617d0addd13f7e5a6dab2b9132
Zhen Wei <zwei@ivani.com>
2016-02-12 19:04:32 -0500

    the obstacle aboidance example project has been done

--- Firmware/Examples/ObstacleAvoidance/main.c
+++ Firmware/Examples/ObstacleAvoidance/main.c
@@ -10,11 +10,14 @@
 #include "ir_sensor_api.h"
 #include "ir_functions.h"
 
+uint8_t ObstacleAvoid(uint16_t frontLimit, uint16_t* sensorData, uint8_t states);
+
 int main(int argc, char const *argv[])
 {
 	uint16_t frontLimit = Calculate_Front_IR_Value(10);
 	uint16_t sensorData[4];
 	int i;
+	uint8_t states = 0;
 
 	printf("Front Limit is %d\n", frontLimit);
 
@@ -22,83 +25,50 @@ int main(int argc, char const *argv[])
 	IRSensorModuleInit(4);
 	printf("Obstacle Avoidance project start!\n");
 
-	robotGo(20,1,20,1);
-
-	for (i = 0; i < 50000; i++);
-	get_ir_datas(sensorData);
-	while (sensorData[0] < frontLimit) {
-		for (i = 0; i < 50000; i++);
+	while(1) {
 		get_ir_datas(sensorData);
+		for(i = 0; i < 50000; i++);
+		states = ObstacleAvoid(frontLimit, sensorData, states);
 	}
-	for (i = 0; i < 50000; i++);
-	robotStop();
-	for (i = 0; i < 50000; i++);
-	robotGet();
 
-	// gpio_t spi_sel;
-	// spi_t spi;
-	// bool value;
-	// uint8_t buf[10] = {'M', ':', 'S', 'T', 'O', 'P', ' ', ' ', ' ', '\n'};
-	// printf("shifted out: %s\n", buf);
-
-	// if (gpio_open(&spi_sel, 17, GPIO_DIR_OUT) < 0) {
-	// 	fprintf(stderr, "gpio_open(): %s\n", gpio_errmsg(&spi_sel));
-	// 	exit(1);
-	// }
-
-	// if (gpio_write(&spi_sel, !value) < 0) {
-	// 	fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(&spi_sel));
-	// 	exit(1);
-	// }
-
-	// if (spi_open(&spi, "/dev/spidev0.0",  0, 10000) < 0) {
-	// 	fprintf(stderr, "spi_open(): %s\n", spi_errmsg(&spi));
-	// 	exit(1);
-	// }
-
-	// if (spi_transfer(&spi, buf, buf, sizeof(buf)) < 0) {
-	// 	fprintf(stderr, "spi_transfer(): %s\n", spi_errmsg(&spi));
-	// 	exit(1);
-	// }
-
-	// printf("shifted in: %s\n", buf);
-
-	// spi_close(&spi);
-
-	// if (gpio_write(&spi_sel, value) < 0) {
-	// 	fprintf(stderr, "gpio_write(): %s\n", gpio_errmsg(&spi_sel));
-	// 	exit(1);
-	// }
-
-	// gpio_close(&spi_sel);
-
-	// printf("%s\n", argv[1]);
-	// if (!strcmp(argv[1],"go")) {
-	// 	if (argc > 3) {
-	// 		uint8_t speed_r = atoi(argv[2]);
-	// 		uint8_t direction_r = atoi(argv[3]);
-	// 		uint8_t speed_l = atoi(argv[4]);
-	// 		uint8_t direction_l = atoi(argv[5]);
-	// 		printf("%d %d %d %d\n",speed_r,direction_r,speed_l,direction_l);
-	// 		robotGo(speed_r,direction_r,speed_l,direction_l);
-	// 	} else {
-	// 		robotGo(50,0,50,0);
-	// 	}
-	// } else if (!strcmp(argv[1],"stop")) {
-	// 	robotStop();
-	// } else if (!strcmp(argv[1], "get")) {
-	// 	robotGet();
-	// } else if (!strcmp(argv[1], "cget")) {
-	// 	robotCGet();
-	// } else if (!strcmp(argv[1], "irget")) {
-	// 	if (argc > 2) {
-	// 	 	printf("IR Sensor %s: %d\n", argv[2], get_ir_data(argv[2]));
-	// 	} else {
-	// 		uint16_t data[4];
-	// 		get_ir_datas(data);
-	// 		printf("F:%d B:%d L:%d R:%d\n", data[0], data[1], data[2], data[3]);
-	// 	}
-		
-	// }
 	return 0;
 }
+
+uint8_t ObstacleAvoid(uint16_t frontLimit, uint16_t* sensorData, uint8_t states) {
+	int i;
+	uint8_t nextStates = states;
+
+	switch (states) {
+		case 0:
+			robotGo(30,1,30,1);
+			for (i = 0; i < 50000; i++);
+			nextStates = 1;
+			break;
+		case 1:
+			if (sensorData[0] > frontLimit) {
+				robotStop();
+				for (i = 0; i < 50000; i++);
+				nextStates = 2;
+			}
+			break;
+		case 2:
+			robotGo(30,0,30,1);//turn right
+			for (i = 0; i < 50000; i++);
+			nextStates = 3;
+			break;
+		case 3:
+			if (sensorData[0] < frontLimit) {
+				robotStop();
+				for (i = 0; i < 50000; i++);
+				nextStates = 0;
+			}
+			break;
+		default:
+			robotStop();
+			for (i = 0; i < 50000; i++);
+			break;
+	}
+
+	return nextStates;
+}
+
